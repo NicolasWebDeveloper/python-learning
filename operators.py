3712225301c7c73f7a5fbe610953acff462a4b76
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-01-24 20:49:48 +0100

    opearators

---
+++ operators.py
@@ -0,0 +1,16 @@
+# There are 7 imortant operators:
+# + Addition
+# - Substraction
+# * Multiplication
+# / Division
+# % Modulus
+# ** Exponentiation
+# // Floor division
+
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(10 / 5)
+print(10 % 3)
+print(10 ** 10)
+print(10 % 3)
